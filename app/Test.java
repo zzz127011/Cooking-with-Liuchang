8d81d3b678c606fc157cf9eabd60a70272b9d6ef
zzz1270 <cathacker1270@gmail.com>
2025-05-09 22:38:09 -0700

    More tests

---
+++ app/Test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Read input from C#
+        String input = scanner.nextLine();
+        
+        // Do some processing in Java
+        String result = "Processed: " + input;
+
+        // Output the result to C#
+        System.out.println(result);
+    }
+}
